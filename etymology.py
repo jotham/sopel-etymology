08829723351aff090d6b5f63f4c8f336df6c3a6c
Aida <autotonic@openmailbox.org>
2018-02-16 05:07:49 -0500

    move request to main func, change to format()

--- etymology.py
+++ etymology.py
@@ -5,18 +5,22 @@ import re, requests, os
 
 BASE_URL = 'https://www.etymonline.com/word/'
 
-def get_definitions(word, html):
+def get_definitions(word):
     definitions = []
-    soup = BeautifulSoup(html, 'html.parser')
-    for element in soup.find_all('div', class_=re.compile('^word--')):
-        try:
-            name = element.find('p', class_=re.compile('^word__name')).text
-        except AttributeError as e:
-            # probably a h1
-            name = element.find('h1', class_=re.compile('^word__name')).text
-        definition = element.find('object').text
-        definitions.append((name, definition))
-    return definitions
+    req = requests.get(BASE_URL + word)
+    if req.ok:
+        soup = BeautifulSoup(req.text, 'html.parser')
+        for element in soup.find_all('div', class_=re.compile('^word--')):
+            try:
+                name = element.find('p', class_=re.compile('^word__name')).text
+            except AttributeError as e:
+                # probably a h1
+                name = element.find('h1', class_=re.compile('^word__name')).text
+            definition = element.find('object').text
+            definitions.append((name, definition))
+        return definitions
+    else:
+        return None
 
 
 try:
@@ -31,16 +35,12 @@ else:
     def f_etymology(bot, trigger):
         """Look up the etymology of a word"""
         phrase = re.sub('[^a-zA-Z ]', '', trigger.group(2)).strip().lower()
-        req = requests.get(BASE_URL + phrase)
-        if req.ok:
-            results = get_definitions(phrase, req.text)
-            if len(results):
-                definitions = ". " .join(["%s %s" % (underline(pair[0]), pair[1]) for pair in results])
-                bot.say(definitions, trigger.sender, len(definitions)*2)
-            else:
-                bot.say('Can\'t find the etymology for "%s".' % phrase, trigger.sender)
+        results = get_definitions(phrase)
+        if len(results):
+            definitions = ". " .join(["{} {}".format(underline(pair[0]), pair[1]) for pair in results])
+            bot.say(definitions, trigger.sender, len(definitions)*2)
         else:
-            bot.say('Can\'t find the etymology for "%s".' % phrase, trigger.sender)
+            bot.say('Can\'t find the etymology for "{}".'.format(phrase), trigger.sender)
         return sopel.module.NOLIMIT
 
 if __name__ == '__main__':
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     print('Looking up "{}"'.format(query))
     results = get_definitions(query)
     if results:
-        definitions = ". ".join(["{} {}".format(pair[1], pair[1]) for pair in results])
+        definitions = ". ".join(["{0} {1}".format(*pair) for pair in results])
         print(definitions)
     else:
         print('Can\'t find the etymology for "{}".'.format(query))
