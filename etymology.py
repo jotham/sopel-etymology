110b2e3b6c603185de8877cfca98ea48cfda6499
Aida <autotonic@openmailbox.org>
2018-02-16 04:49:29 -0500

    add template for cli

--- etymology.py
+++ etymology.py
@@ -1,11 +1,9 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
-from sopel.module import commands, example, NOLIMIT
-from sopel.formatting import underline
 from bs4 import BeautifulSoup
-import re, requests
+import re, requests, os
 
-WORD_URI = 'https://www.etymonline.com/word/'
+BASE_URL = 'https://www.etymonline.com/word/'
 
 def get_definitions(word, html):
     definitions = []
@@ -20,19 +18,40 @@ def get_definitions(word, html):
         definitions.append((name, definition))
     return definitions
 
-@commands('ety')
-@example('.ety word or phrase')
-def f_etymology(bot, trigger):
-    """Look up the etymology of a word"""
-    phrase = re.sub('[^a-zA-Z ]', '', trigger.group(2)).strip().lower()
-    req = requests.get(WORD_URI + phrase)
-    if req.ok:
-        results = get_definitions(phrase, req.text)
-        if len(results):
-            definitions = ". " .join(["%s %s" % (underline(pair[0]), pair[1]) for pair in results])
-            bot.say(definitions, trigger.sender, len(definitions)*2)
+
+try:
+    import sopel.module
+except ImportError:
+    pass
+else:
+    from sopel.formatting import underline
+
+    @sopel.module.commands('ety')
+    @sopel.module.example('.ety word or phrase')
+    def f_etymology(bot, trigger):
+        """Look up the etymology of a word"""
+        phrase = re.sub('[^a-zA-Z ]', '', trigger.group(2)).strip().lower()
+        req = requests.get(BASE_URL + phrase)
+        if req.ok:
+            results = get_definitions(phrase, req.text)
+            if len(results):
+                definitions = ". " .join(["%s %s" % (underline(pair[0]), pair[1]) for pair in results])
+                bot.say(definitions, trigger.sender, len(definitions)*2)
+            else:
+                bot.say('Can\'t find the etymology for "%s".' % phrase, trigger.sender)
         else:
             bot.say('Can\'t find the etymology for "%s".' % phrase, trigger.sender)
+        return sopel.module.NOLIMIT
+
+if __name__ == '__main__':
+    import sys
+    query = 'cunt'
+    if len(sys.argv) > 1:
+        query = ' '.join(sys.argv[1:])
+    print('Looking up "{}"'.format(query))
+    results = get_definitions(query)
+    if results:
+        definitions = ". ".join(["{} {}".format(pair[1], pair[1]) for pair in results])
+        print(definitions)
     else:
-        bot.say('Can\'t find the etymology for "%s".' % phrase, trigger.sender)
-    return NOLIMIT
+        print('Can\'t find the etymology for "{}".'.format(query))
